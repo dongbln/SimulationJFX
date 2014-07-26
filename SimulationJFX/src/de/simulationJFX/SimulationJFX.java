357330342b288261e0728e2d30fcbe9f328fc9bb
ts <ts@laptop>
2014-07-26 20:35:36 +0200

    Init project

---
+++ SimulationJFX/src/de/simulationJFX/SimulationJFX.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.simulationJFX;
+
+/**
+ * @author ts
+ *
+ */
+public class SimulationJFX {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
